7531a780205eea6d01316603dbf0afdb3f93d28a
kevin01157007 <k0920687524@gmail.com>
2023-12-22 20:03:06 +0800

    增加30秒

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
   const express = require('express')
   const app = express()
   const mongoose = require('mongoose');
-  mongoose.connect(process.env.MONGO_URL);
+  mongoose.connect(process.env.MONGO_URL + '?connectTimeoutMS=30000');
   const db = mongoose.connection;
 
 // 與資料庫連線發生錯誤時
