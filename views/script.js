52e898d42b59369742219a27bac66391d9a8d2c9
kevin01157007 <k0920687524@gmail.com>
2023-12-22 17:12:31 +0800

    刪掉script

--- views/script.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    setInterval(function() {
-        document.getElementById('goboard').classList.toggle('fadeInOut');
-        document.getElementById('goboard1').classList.toggle('fadeInOut');
-    }, 4000);
-};
