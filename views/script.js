18932c890d1d1123960adfc3be0882c8474363ea
kevin01157007 <k0920687524@gmail.com>
2023-12-22 17:11:04 +0800

    改

--- views/script.js
+++ views/script.js
@@ -3,13 +3,4 @@ window.onload = function() {
         document.getElementById('goboard').classList.toggle('fadeInOut');
         document.getElementById('goboard1').classList.toggle('fadeInOut');
     }, 4000);
-    document.querySelector('.hamburger').addEventListener('click', function() {
-        var sidebar = document.querySelector('.sidebar');
-        var width = getComputedStyle(sidebar).width;
-        if (width === '0px') {
-            sidebar.style.width = '250px';
-        } else {
-            sidebar.style.width = '0px';
-        }
-    });
 };
