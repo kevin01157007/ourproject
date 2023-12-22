11ee834b25733939da647d2db6f94dd2b834747c
kevin01157007 <k0920687524@gmail.com>
2023-12-22 16:33:01 +0800

    名人介紹

--- celebrity/celebrity.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.hamburger').addEventListener('click', function() {
-    var sidebar = document.querySelector('.sidebar');
-    var width = getComputedStyle(sidebar).width;
-    if (width === '0px') {
-        sidebar.style.width = '250px';
-    } else {
-        sidebar.style.width = '0px';
-    }
-});
