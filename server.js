8b93c7068bf932713c22e1fdf49cb4f4e409b664
kevin01157007 <k0920687524@gmail.com>
2023-12-22 16:38:44 +0800

    env var

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ const User = mongoose.model('User', userSchema);
   app.use(express.urlencoded({ extended: false }))
   app.use(flash())
   app.use(session({
-    secret: 'your secret string',
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true
   }));
